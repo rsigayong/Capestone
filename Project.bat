a9010ec45eebb6ae3e18698aab04716fc541d8d6
Raiwaso Sigayong <169107662+rsigayong@users.noreply.github.com>
2024-06-10 10:16:56 +1000

    Create Project.bat

---
+++ Project.bat
@@ -0,0 +1,9 @@
+@echo off
+color 0a
+cls
+echo /////////////////////////////////////////////////////////////////
+echo This Script is created purposely for CEIT CAPESTONE PROJECT 
+echo CEIT @ 2024
+echo Copy Right Raiwaso Sigayong 2024, All rights reserved 
+echo /////////////////////////////////////////////////////////////////
+echo.
