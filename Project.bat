976f6035bf3da7e1a28395b59eeed780350d603b
Raiwaso Sigayong <169107662+rsigayong@users.noreply.github.com>
2024-06-20 08:36:09 +1000

    Update Project.bat
    Update Project correct capstone name

--- Project.bat
+++ Project.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 echo /////////////////////////////////////////////////////////////////
-echo This Script is created purposely for CEIT CAPESTONE PROJECT 
+echo This Script is created purposely for CEIT CAPSTONE PROJECT 
 echo CEIT @ 2024
 echo Copy Right Raiwaso Sigayong 2024, All rights reserved 
 echo /////////////////////////////////////////////////////////////////
