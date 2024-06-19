638991d871b66324979a3fb987a59e60439c6ca8
Raiwaso Sigayong <169107662+rsigayong@users.noreply.github.com>
2024-06-20 08:46:10 +1000

    Update Project.bat

--- Project.bat
+++ Project.bat
@@ -1,8 +1,8 @@
 @echo off
 cls
 echo /////////////////////////////////////////////////////////////////
-echo This Script is created purposely for CEIT CAPSTONE PROJECT 
+echo This Script is created purposely for CEIT CAPSTONE PROJECT.
 echo CEIT @ 2024
-echo Copy Right Raiwaso Sigayong 2024, All rights reserved 
+echo Copy Right Raiwaso Sigayong 2024, All rights reserved. 
 echo /////////////////////////////////////////////////////////////////
 echo.
